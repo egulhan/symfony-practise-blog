bd40079581c9e4829cef39e9b31c0c7abb5af65b
Erman Gülhan <mr.ermangulhan@gmail.com>
2019-04-19 08:15:46 +0300

    init

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eg
+ * Date: 4/16/2019
+ * Time: 9:23 PM
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SiteController extends AbstractController
+{
+}
